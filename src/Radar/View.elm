10128fe1a0113eb8105bbcc4e72b520fc57c6234
Charlie Koster <ckoster22@gmail.com>
2017-05-25 22:21:23 -0500

    Drawing all rings

--- src/Radar/View.elm
+++ src/Radar/View.elm
@@ -6,11 +6,102 @@ import Svg exposing (Attribute, Svg, path, svg)
 import Svg.Attributes exposing (cx, cy, d, fill, height, r, width)
 
 
+type RingQuadrant
+    = UpperRight
+    | LowerRight
+    | LowerLeft
+    | UpperLeft
+
+
+radarCx : Float
+radarCx =
+    400
+
+
+radarCy : Float
+radarCy =
+    400
+
+
+ring1Color : String
+ring1Color =
+    "#BABABA"
+
+
+ringPadding : Float
+ringPadding =
+    5
+
+
+ring1Radius : Float
+ring1Radius =
+    150
+
+
+ring2Color : String
+ring2Color =
+    "#CACACA"
+
+
+ring2Radius : Float
+ring2Radius =
+    250
+
+
+ring3Color : String
+ring3Color =
+    "#DADADA"
+
+
+ring3Radius : Float
+ring3Radius =
+    325
+
+
+ring4Color : String
+ring4Color =
+    "#EEEEEE"
+
+
+ring4Radius : Float
+ring4Radius =
+    375
+
+
 view : Radar -> Html msg
 view radar =
     svg
-        [ width "600px", height "600px" ]
-        [ path [ arc 150 150 150 90 180, fill "#FF0000" ] [] ]
+        [ width "800px", height "800px" ]
+        (fourRings ring1Radius ring1Color
+            |> List.append (fourRings ring2Radius ring2Color)
+            |> List.append (fourRings ring3Radius ring3Color)
+            |> List.append (fourRings ring4Radius ring4Color)
+        )
+
+
+fourRings : Float -> String -> List (Svg msg)
+fourRings ringRadius ringColor =
+    [ ring UpperRight ringRadius ringColor
+    , ring LowerRight ringRadius ringColor
+    , ring LowerLeft ringRadius ringColor
+    , ring UpperLeft ringRadius ringColor
+    ]
+
+
+ring : RingQuadrant -> Float -> String -> Svg msg
+ring quadrant ringRadius ringColor =
+    case quadrant of
+        UpperRight ->
+            path [ arc (radarCx + ringPadding) (radarCy - ringPadding) (ringRadius - ringPadding) 0 90, fill ringColor ] []
+
+        LowerRight ->
+            path [ arc (radarCx + ringPadding) (radarCy + ringPadding) (ringRadius - ringPadding) 90 180, fill ringColor ] []
+
+        LowerLeft ->
+            path [ arc (radarCx - ringPadding) (radarCy + ringPadding) (ringRadius - ringPadding) 180 270, fill ringColor ] []
+
+        UpperLeft ->
+            path [ arc (radarCx - ringPadding) (radarCy - ringPadding) (ringRadius - ringPadding) 270 360, fill ringColor ] []
 
 
 arc : Float -> Float -> Float -> Float -> Float -> Attribute msg
