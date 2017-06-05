ba70dcfc6c1578e165de603582b264b23734a1a9
Charlie Koster <ckoster22@gmail.com>
2017-06-04 21:10:01 -0500

    Using TEA

--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,114 @@
 module Main exposing (main)
 
-import Html
-import Radar.Model as RadarModel
+import Html exposing (Html, button, text)
+import Html.Events exposing (onClick)
+import Radar.Model exposing (Quadrant(..), Radar, Ring(..))
 import Radar.View as RadarView
 
 
-main : Program Never RadarModel.Radar Msg
+main : Program Never AppState Msg
 main =
     Html.program
-        { init = [] ! []
-        , update = \_ model -> model ! []
+        { init = init
+        , update = update
         , subscriptions = \_ -> Sub.none
-        , view = RadarView.view
+        , view = view
         }
 
 
 type Msg
-    = Msg
+    = ShowMockData
+
+
+type AppState
+    = Init
+    | ShowRadar Radar
+
+
+init : ( AppState, Cmd Msg )
+init =
+    Init ! []
+
+
+update : Msg -> AppState -> ( AppState, Cmd Msg )
+update msg appState =
+    case msg of
+        ShowMockData ->
+            ShowRadar mockRadar ! []
+
+
+view : AppState -> Html Msg
+view appState =
+    case appState of
+        Init ->
+            button
+                [ onClick ShowMockData ]
+                [ text "Show mock radar" ]
+
+        ShowRadar radar ->
+            RadarView.view radar
+
+
+mockRadar : Radar
+mockRadar =
+    [ { name = "tech1"
+      , ring = Hold
+      , quadrant = Tools
+      , isNew = False
+      , description = "description"
+      }
+    , { name = "tech2"
+      , ring = Hold
+      , quadrant = Tools
+      , isNew = False
+      , description = "description"
+      }
+    , { name = "tech3"
+      , ring = Hold
+      , quadrant = Tools
+      , isNew = False
+      , description = "description"
+      }
+    , { name = "tech4"
+      , ring = Hold
+      , quadrant = Tools
+      , isNew = False
+      , description = "description"
+      }
+    , { name = "tech5"
+      , ring = Hold
+      , quadrant = Tools
+      , isNew = False
+      , description = "description"
+      }
+    , { name = "tech6"
+      , ring = Hold
+      , quadrant = Tools
+      , isNew = False
+      , description = "description"
+      }
+    , { name = "tech7"
+      , ring = Hold
+      , quadrant = Tools
+      , isNew = False
+      , description = "description"
+      }
+    , { name = "tech8"
+      , ring = Hold
+      , quadrant = Tools
+      , isNew = False
+      , description = "description"
+      }
+    , { name = "tech2"
+      , ring = Hold
+      , quadrant = Tools
+      , isNew = False
+      , description = "description"
+      }
+    , { name = "tech3"
+      , ring = Hold
+      , quadrant = Tools
+      , isNew = False
+      , description = "description"
+      }
+    ]
